fcd9dff134d8889b49bb8c1e5c66073afa1e8649
HoaiNhu <22521051@gm.uit.edu.vn>
2024-12-07 13:08:17 +0700

    district

--- src/routes/WardRouer.js
+++ src/routes/WardRouer.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require("../middleware/authMiddleware");
 router.post("/create-ward", authMiddleware, wardController.createWard);
 router.put("/update-ward/:id", authMiddleware, wardController.updateWard);
 router.delete("/delete-ward/:id", authMiddleware, wardController.deleteWard);
-router.get("/get-detail-ward/:id", wardController.getDetailWard);
+router.get("/get-detail-ward/:id", wardController.getDetailsWard);
 router.get("/get-all-ward", wardController.getAllWard);
 
 module.exports = router;
